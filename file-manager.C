4a935e3dd52ca749e052d30c70e951d59871a077
goal-chaser <akhilsain2222@gmail.com>
2025-10-30 22:34:55 +0530

    Prototype for editing a existing file

--- file-manager.C
+++ file-manager.C
@@ -1,7 +1,6 @@
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
-int create_file(char x[]){
+int create_file(char x[],char sc[]){
     FILE *pfile = fopen(x,"w");
     if(pfile == NULL){
         printf("Error in writing the file.");
@@ -28,12 +27,52 @@ int create_file(char x[]){
 
 }
 
+int open_file(char nf[],char sc[]){
+    char buffer[4096] = {0};
+    FILE *popfile = fopen(nf, "r");
+    if(strcmp(sc, "-r") == 0){
+        FILE *popfile = fopen(nf, "r");
+        while(fgets(buffer, sizeof(buffer), popfile) != NULL){
+            printf("%s",buffer);
+        }
+        printf("\n");
+    }
+    if(popfile == NULL){
+        printf("Error opening a file.");
+        return 1;
+    }
+    if(strcmp(sc, "-rw") == 0){
+        char bufferw[4096];
+        popfile = fopen(nf,"r+");
+        printf("--------------------------------------");
+        while(1 == 1){
+            int i;
+            fgets(bufferw, 4096, stdin);
+            i = strcspn(bufferw, "\n");
+            bufferw[i] = '\0';
+            if(strcmp(bufferw, "^e") == 0){
+                printf("\n file has been updated");
+                break;
+            }
+            fprintf(popfile, "%s\n", bufferw);
+        }
+    }
+
+    fclose(popfile);
+    return 0;
+}
+
 int main(){
-    char c[10],nf[24];
+    char c[10],nf[24],sc[4];
     printf("> ");
-    scanf("%[a-zA-z] %[a-zA-Z.]",c,nf);
-    if(strcmp(c, "create") == 0){
-        create_file(nf);
+    scanf("%[a-zA-Z] %[a-zA-Z.] %[ a-zA-Z-]",c,nf,sc);
+    if(strcmp(c, "file") == 0){
+        if(strcmp(sc, "-c") == 0){
+            create_file(nf,sc);
+        }
+        else if(strcmp(sc, "-r") == 0|| strcmp(sc, "-rw") == 0){
+            open_file(nf,sc);
+        }
     }
     else{
         printf("Invalid command");
