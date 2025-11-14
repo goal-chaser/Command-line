4fb0d10020647aaffe8827f5f3cf2d5cc1f5ac87
goal-chaser <akhilsain2222@gmail.com>
2025-11-14 13:40:39 +0530

    Completion of directory code wihtout the previous action

--- file-manager.C
+++ file-manager.C
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
-
+#include<dirent.h>
+#include<errno.h>
+#include<stdbool.h>
 int create_file(char x[]){
     FILE *pfile = fopen(x,"w");
     if(pfile == NULL){
@@ -17,7 +19,7 @@ int create_file(char x[]){
         fgets(buffer, sizeof(buffer), stdin);
         i = strcspn(buffer, "\n");
         buffer[i] = '\0';
-        if(strcmp(buffer, "^e") == 0){
+        if(strcspn(buffer, "^e") == 0){
             printf("\n");
             break;
         }
@@ -49,14 +51,14 @@ int read_file(char nf[]){
 int edit_file(char nf[]){
     FILE *popfile = fopen(nf, "a");
         char bufferw[4096];
-        printf("--------------------------------------");
+        printf("--------------------------------------\n");
         while(1 == 1){
             printf("> ");
             int i;
             fgets(bufferw, 4096, stdin);
             i = strcspn(bufferw, "\n");
             bufferw[i] = '\0';
-            if(strcmp(bufferw, "^e") == 0){
+            if(strcspn(bufferw, "^e") == 0){
                 printf("\n file has been updated");
                 break;
             }
@@ -67,41 +69,76 @@ int edit_file(char nf[]){
     }
 
 
+int list(char *x){
+    struct dirent *list;
+    DIR *dir = opendir(x);
+    if(dir == NULL){
+        perror("opendir failed");
+        return 1;
+    }
+    
+    while((list = readdir(dir)) != NULL){
+        printf("%s \n",list -> d_name);
+    }
+    if(errno != 0){
+        perror("readdir failed");
+        return 1;
+    }
+    if(closedir(dir) != 0){
+        perror("close dir failed");
+    }
+
+    return 0;
+}
+
+void change_directory(char *y){
+    int check;
+    check = chdir(y);
+    if(check != 0){
+        perror("error in chdir");
+    }
+}
 
 int main(){
+    while(1 == 1){
     char wd[1024];
     char input[100];
     if(fgets(input, sizeof(input), stdin) != NULL){
         input[strcspn(input, "\n")] = '\0';
         char *token = strtok(input, " ");
-        while(token != NULL){
-            if(strcmp(token, "file") == 0){
+        if(strcmp(token, "exit") == 0){
+            break;
+        }
+        if(strcmp(token, "file") == 0){
+            token = strtok(NULL, " ");
+            if(strcmp(token, "-c") == 0){
                 token = strtok(NULL, " ");
-                if(strcmp(token, "-c") == 0){
-                    token = strtok(NULL, " ");
-                    create_file(token);
-                    break;
-                }
-                else if(strcmp(token, "-r") == 0){
-                    token = strtok(NULL, " ");
-                    read_file(token);
-                    break;
-                }
-                else if(strcmp(token, "-rw") == 0){
-                    token = strtok(NULL, " ");
-                    edit_file(token);
-                    break;
-                }
-                else{
-                    printf("Invalid command");
-                }
+                create_file(token);
             }
-            if(strcmp(token, "pwd") == 0){
-                getcwd(wd, sizeof(wd));
-                printf("%s\n",wd);
-                break;
+            else if(strcmp(token, "-r") == 0){
+                token = strtok(NULL, " ");
+                read_file(token);
             }
+            else if(strcmp(token, "-rw") == 0){
+                token = strtok(NULL, " ");
+                edit_file(token);
+            }
+            else{
+                printf("Invalid command");
+            }
+        }
+        if(strcmp(token, "pwd") == 0){
+            getcwd(wd, sizeof(wd));
+            printf("%s\n",wd);
+        }
+        if(strcmp(token, "ls") == 0){
+            getcwd(wd, sizeof(wd));
+            list(wd);
+        }
+        if(strcmp(token, "cd") == 0){
+            token = strtok(NULL,"");
+            change_directory(token);
         }
     }
 }
-
+}
